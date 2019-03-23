5d36e4d2eb4cdf388b28f799d6a7d1c6b09b6fca
Thomas Pitts <Tom@Glorfindel.local>
2019-03-23 19:45:57 +0000

    first commit

--- Wildfire1.1/PayViewController.swift
+++ Wildfire1.1/PayViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class PayViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Wildfire1.1/QRViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QRViewController.swift
+//  Wildfire1.1
+//
+//  Created by Thomas Pitts on 19/01/2019.
+//  Copyright © 2019 Wildfire. All rights reserved.
+//
+
+import UIKit
+
+class QRViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
