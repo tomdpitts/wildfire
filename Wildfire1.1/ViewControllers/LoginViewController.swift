39e7f30ce82e7ccbe9755aa8dcb904c5affde4e7
Tom Daniel <tom.daniel.gb@gmail.com>
2019-08-22 16:28:25 +0200

    adds foundational login screens

---
+++ Wildfire1.1/ViewControllers/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  Wildfire1.1
+//
+//  Created by Thomas Pitts on 22/08/2019.
+//  Copyright © 2019 Wildfire. All rights reserved.
+//
+
+import UIKit
+///import FirebaseUI
+
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+//    var actionCodeSettings = ActionCodeSettings()
+//    actionCodeSettings.url = URL(string: "https://example.appspot.com")
+//    actionCodeSettings.handleCodeInApp = true
+//    actionCodeSettings.setAndroidPackageName("com.firebase.example", installIfNotAvailable: false, minimumVersion: "12")
+//
+//    let provider = FUIEmailAuth.initAuthAuthUI(FUIAuth.defaultAuthUI(), signInMethod: FIREmailLinkAuthSignInMethod, forceSameDevice: false, allowNewEmailAccounts: true, actionCodeSetting: actionCodeSettings)
+//
+//
+//    @IBAction func loginButtonTapped(_ sender: UIButton) {
+//
+//        // get the default auth UI object
+//
+//        // Set ourselves as the delegate
+//
+//        // get a reference to the auth UI view controller
+//
+//        // Show it
+//
+//    }
+    
+}
