5f62cef07b42031c208c6546fedee825c24c076d
Tom Daniel <tom.daniel.gb@gmail.com>
2019-08-26 16:45:00 +0200

    new pods and first login addedd

--- Podfile
+++ Podfile
@@ -1,12 +1,15 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '12.4'
 
 target 'Wildfire1.1' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for Wildfire1.1
+pod 'Firebase/Analytics'
 pod 'Firebase/Core'
+pod 'FirebaseUI'
+pod 'Firebase/Auth'
 pod 'Firebase/Database'
   target 'Wildfire1.1Tests' do
     inherit! :search_paths
--- Wildfire1.1/ViewControllers/LoginViewController.swift
+++ Wildfire1.1/ViewControllers/LoginViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-///import FirebaseUI
+import FirebaseUI
 
 
 class LoginViewController: UIViewController {
@@ -24,18 +24,40 @@ class LoginViewController: UIViewController {
 //    actionCodeSettings.setAndroidPackageName("com.firebase.example", installIfNotAvailable: false, minimumVersion: "12")
 //
 //    let provider = FUIEmailAuth.initAuthAuthUI(FUIAuth.defaultAuthUI(), signInMethod: FIREmailLinkAuthSignInMethod, forceSameDevice: false, allowNewEmailAccounts: true, actionCodeSetting: actionCodeSettings)
-//
-//
-//    @IBAction func loginButtonTapped(_ sender: UIButton) {
-//
-//        // get the default auth UI object
-//
-//        // Set ourselves as the delegate
-//
-//        // get a reference to the auth UI view controller
-//
-//        // Show it
-//
-//    }
+
+
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+
+        // get the default auth UI object
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        guard authUI != nil else {
+            // Log the error
+            return
+        }
+        // Set ourselves as the delegate
+        authUI?.delegate = self
+        
+        // get a reference to the auth UI view controller
+        let authViewController = authUI!.authViewController()
+        
+        // Show it
+        present(authViewController, animated: true, completion: nil)
+    }
     
+}
+
+extension LoginViewController: FUIAuthDelegate {
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        
+        // check for errors
+        guard error == nil else {
+            // log error
+            return
+        }
+        
+        // authDataResult?.user.uid
+        performSegue(withIdentifier: "goHome", sender: self)
+    }
 }
