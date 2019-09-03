acd641e310e11da355972267e98ef12dbd9d6c91
Tom Daniel <tom.daniel.gb@gmail.com>
2019-09-03 14:43:48 +0100

    adds firebase cli files

--- Wildfire1.1/AppDelegate.swift
+++ Wildfire1.1/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        // one for later
+        // let db = Firestore.firestore()
         return true
     }
 
