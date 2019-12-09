07768e6a018e4fce2f5883bfb65c597b868a5aa5
Tom Daniel <tom.daniel.gb@gmail.com>
2019-12-09 12:56:18 +1100

    saving progress

--- Wildfire1.1/ViewControllers/Account2ViewController.swift
+++ Wildfire1.1/ViewControllers/Account2ViewController.swift
@@ -12,19 +12,28 @@ class Account2ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.title = "Account"
+        navigationController?.navigationBar.prefersLargeTitles = true
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath, cellText: String) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")! //1.
+            
+         let text = cellText //2.
+            
+         cell.textLabel?.text = text //3.
+            
+         return cell //4.
+    }
 
 }
--- Wildfire1.1/ViewControllers/ContactsViewController.swift
+++ Wildfire1.1/ViewControllers/ContactsViewController.swift
@@ -26,6 +26,7 @@ class ContactsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // can't remember what this does - try google
         navigationController?.interactivePopGestureRecognizer?.delegate = nil
         
         navigationItem.title = "Send"
