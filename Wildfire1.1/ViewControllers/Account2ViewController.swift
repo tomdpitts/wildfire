6375b422bdcdb2f8fad1f10d51ab565eae0be70a
Tom Daniel <tom.daniel.gb@gmail.com>
2019-12-10 13:25:17 +1100

    account page outline structure added

--- Wildfire1.1/ViewControllers/Account2ViewController.swift
+++ Wildfire1.1/ViewControllers/Account2ViewController.swift
@@ -10,30 +10,99 @@ import UIKit
 
 class Account2ViewController: UITableViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "Account"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        
+        tableView.tableFooterView = UIView()
+        tableView.backgroundColor = .groupTableViewBackground
 
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         return 5
+         return 10
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath, cellText: String) -> UITableViewCell {
-         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")! //1.
-            
-         let text = cellText //2.
-            
-         cell.textLabel?.text = text //3.
-            
-         return cell //4.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "separator0Cell")!
+            return cell
+        } else if indexPath.row == 1 {
+             // User Profile Cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "balanceCell")!
+//            let text = "cellText" //2.
+//
+//            cell.textLabel?.text = text //3.
+            return cell
+        }
+        else if indexPath.row == 2 {
+             // User Profile Cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "userIDCell")!
+//            let text = "cellText" //2.
+//
+//            cell.textLabel?.text = text //3.
+            return cell
+        } else if indexPath.row == 3 {
+             // User Profile Cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "separator1Cell")!
+//            let text = "cellText" //2.
+//
+//            cell.textLabel?.text = text //3.
+            return cell
+        } else if indexPath.row == 4 {
+             // User Profile Cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "receiptsCell")!
+//            let text = "cellText" //2.
+//
+//            cell.textLabel?.text = text //3.
+            return cell
+        } else if indexPath.row == 5 {
+             // User Profile Cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "separator2Cell")!
+//            let text = "cellText" //2.
+//
+//            cell.textLabel?.text = text //3.
+            return cell
+        } else if indexPath.row == 6 {
+             // User Profile Cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "paymentMethodsCell")!
+//            let text = "cellText" //2.
+//
+//            cell.textLabel?.text = text //3.
+            return cell
+        } else if indexPath.row == 7 {
+             // User Profile Cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "depositToBankCell")!
+//            let text = "cellText" //2.
+//
+//            cell.textLabel?.text = text //3.
+            return cell
+        } else if indexPath.row == 8 {
+             // User Profile Cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "separator3Cell")!
+//            let text = "cellText" //2.
+//
+//            cell.textLabel?.text = text //3.
+            return cell
+        } else if indexPath.row == 9 {
+             // User Profile Cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "deleteAccountCell")!
+//            let text = "cellText" //2.
+//
+//            cell.textLabel?.text = text //3.
+            return cell
+        } else {
+             //configure cell type 2
+            let cell = tableView.dequeueReusableCell(withIdentifier: "separator3Cell")!
+            return cell
+        }
     }
-
 }
--- Wildfire1.1/ViewControllers/AccountViewController.swift
+++ Wildfire1.1/ViewControllers/AccountViewController.swift
@@ -92,23 +92,23 @@ class AccountViewController: UIViewController {
     })
     }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         return 5
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath, cellText: String) -> UITableViewCell {
-         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")! //1.
-            
-         let text = cellText //2.
-            
-         cell.textLabel?.text = text //3.
-            
-         return cell //4.
-    }
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 2
+//    }
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//         return 5
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath, cellText: String) -> UITableViewCell {
+//         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")! //1.
+//            
+//         let text = cellText //2.
+//            
+//         cell.textLabel?.text = text //3.
+//            
+//         return cell //4.
+//    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
